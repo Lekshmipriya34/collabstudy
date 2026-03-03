69a6acd99808f8d8c6026c4fd7b6047330170cda
UttaraPraveen <uttarapraveen2005@gmail.com>
2026-03-03 18:44:18 +0530

    securing firebase apis in env file

--- collaborative-study-room/src/firebase.js
+++ collaborative-study-room/src/firebase.js
@@ -1,15 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// Your web app's Firebase configuration
-// (You get this from the Firebase Console > Project Settings)
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCslKLtlY1nEBm-IZNQND1UetSbzfJK1E0",
-  authDomain: "collab-study-app-419b9.firebaseapp.com",
-  projectId: "collab-study-app-419b9",
-  storageBucket: "collab-study-app-419b9.firebasestorage.app",
-  messagingSenderId: "65221881804",
-  appId: "1:65221881804:web:8784c2ea84faf6651cb769"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
